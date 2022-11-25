50735626973addb8f7ccb978bd4aa6757b4fa9c8
dereklim97 <68524765+dereklim97@users.noreply.github.com>
2022-11-26 04:22:09 +0800

    Add files via upload
    updated and included description with screenshot

--- Question 2/q2.cpp
+++ Question 2/q2.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-void quadProbe(int arr[], int arrSize, int ht[], int htSize, int m) {
+void quadProbe(int arr[], int arrSize, int ht[], int htSize) {
     for (int i = 0; i < arrSize; i++) {
-        int hv = arr[i] % m; // compute the hash value
+        int hv = arr[i] % htSize; // compute the hash value
         if (ht[hv] == -1)
             // no collision, insert into ht
             ht[hv] = arr[i];
@@ -10,7 +10,7 @@ void quadProbe(int arr[], int arrSize, int ht[], int htSize, int m) {
             // collision occurred, insert into hv
             // using quad prob hv2 value
             for (int j = 1; j < htSize; j++) {
-                int hv2 = (hv + (j * j)) % m;
+                int hv2 = (hv + (j * j)) % htSize;
                 if (ht[hv2] == -1) {
                     // insert into hv if there's no collision
                     ht[hv2] = arr[i];
@@ -22,15 +22,14 @@ void quadProbe(int arr[], int arrSize, int ht[], int htSize, int m) {
 }
 
 int main() {
-    int arr[] = { 76,40,48,5,55}, arrSize =5 ;
-    int ht[7], htSize = 7;
-    int m = 7;
+    int arr[] = {77, 94, 74, 90, 55, 51, 54, 76, 103}, arrSize = sizeof(arr)/sizeof(arr[0]);
+    int ht[13], htSize = sizeof(ht)/sizeof(ht[0]);
     // Initializing the hash table
     for (int i = 0; i < htSize; i++) {
         ht[i] = -1;
     }
     // Function call
-    quadProbe(arr, arrSize, ht, htSize, m);
+    quadProbe(arr, arrSize, ht, htSize);
     // Print result
     for (int i = 0; i < htSize; i++) {
         std::cout << ht[i] << " ";
